d0907c6e3af55347da28ad349378c68618016bc6
Shanreed25 <tralainereed@gmail.com>
2025-09-06 17:35:52 -0700

    added css display property

--- server.py
+++ server.py
@@ -7,9 +7,16 @@ def home():
     return render_template("index.html")
 
 
+#****************************CSS Routes*****************************************
 @app.route("/css")
 def css_home():
     return render_template("css/css_home.html")
+
+@app.route("/css_concept/display")
+def css_display():
+    return render_template("./css/fundamentals/display.html")
+
+
 
 
 if __name__ == "__main__":
